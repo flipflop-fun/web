755aa3f4e6426e6d336684c7b23eb7839e47513a
flipflop-fun <flipflopdotfun@gmail.com>
2026-03-22 21:57:10 +0800

    fix: add vm-browserify polyfill to craco config to fix Vercel build

--- craco.config.js
+++ craco.config.js
@@ -14,6 +14,7 @@ module.exports = {
                 zlib: require.resolve('browserify-zlib'),
                 buffer: require.resolve('buffer'),
                 process: require.resolve('process/browser.js'),
+                vm: require.resolve('vm-browserify'),
             };
 
             webpackConfig.devtool = false;
